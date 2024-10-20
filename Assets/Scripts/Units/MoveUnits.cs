77acd59130858e78a397dbfdd80a19dcef2a80f9
HyanJF <hyan.fierros@uniat.edu.mx>
2024-10-20 01:23:42 -0600

    delete UnitsMovment
    Movement Basic Finish

--- Assets/Scripts/Units/MoveUnits.cs
+++ Assets/Scripts/Units/MoveUnits.cs
@@ -9,9 +9,35 @@ public class MoveUnits : MonoBehaviour
     public GameObject finalPosition;
     public float speed;
 
-    
+    private void OnMouseDown()
+    {
+        UnitListController.uLController.SelectUnit(this);
+    }
+
     private void Update()
     {
-        unit.transform.position = Vector3.MoveTowards(unit.transform.position, finalPosition.transform.position, speed * Time.deltaTime);
+        if (finalPosition != null && IsPositionInRange(finalPosition.transform.position))
+        {
+            unit.transform.position = Vector3.MoveTowards(unit.transform.position, finalPosition.transform.position, speed * Time.deltaTime);
+
+            if (unit.transform.position == finalPosition.transform.position)
+            {
+                UnitManager.unitManager.ClearHighlight(); 
+                UnitManager.unitManager.DeselectUnit(); 
+                finalPosition = null;  
+            }
+        }
+    }
+
+    public void UpdateFinalPosition(GameObject newPosition)
+    {
+        finalPosition = newPosition;
+    }
+
+    private bool IsPositionInRange(Vector3 targetPosition)
+    {
+        Vector3 currentPosition = unit.transform.position;
+        float distance = Vector3.Distance(currentPosition, targetPosition);
+        return distance <= UnitManager.unitManager.highlightRange;
     }
 }
