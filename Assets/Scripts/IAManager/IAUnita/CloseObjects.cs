f4952c60b32c94d0016c13ca72349f044e80b02e
Jared Osorno <angel.osorno@uniat.edu.mx>
2024-11-10 18:47:40 -0600

    working on

---
+++ Assets/Scripts/IAManager/IAUnita/CloseObjects.cs
@@ -0,0 +1,14 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CloseObjects : MonoBehaviour
+{
+    [SerializeField] List<GameObject> enemiesClose;
+    [SerializeField] List<GameObject> allyClose;
+    [SerializeField] List<GameObject> obstaclesClose;
+
+    public void UpdateCLoseObjects()
+    {
+        
+    }
+}
